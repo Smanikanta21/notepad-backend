a09a9d23e01b983f5a71fe5eb90c7e83e61ca9df
Smanikanta72 <siraparapuabhinay21@gmail.com>
2025-07-24 23:57:59 +0530

    Initialize passport for authentication in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const connectDB = require('../database/dbConfig')
 const express = require('express')
 const auth = require('../auth/auth')
 const app = express()
+const passport = require('./auth/passport')
 const cors = require('cors')
 require('dotenv').config()
 app.use(express.json())
@@ -9,6 +10,8 @@ app.use(cors({
     origin : ['http://localhost:5173', "https://note-abqtry4cv-smanikanta21s-projects.vercel.app"],
     credentials : true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use('/auth' , auth)
 const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
