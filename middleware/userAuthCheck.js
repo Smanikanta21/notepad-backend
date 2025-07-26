15c32080b88c1bcbda7d0d123973667a4f295823
Smanikanta72 <siraparapuabhinay21@gmail.com>
2025-07-26 14:24:36 +0530

    Fix case sensitivity in User model import in userAuthCheck.js

--- middleware/userAuthCheck.js
+++ middleware/userAuthCheck.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken')
-const User = require('../models/user');
+const User = require('../models/User');
 
 const userAuthCheck = async(req,res,next) =>{
     const { token } = req.cookies;
