c4a0f924cbc33a6adcfaeabfc2dbc51b214dfa08
Smanikanta72 <siraparapuabhinay21@gmail.com>
2025-07-26 14:22:29 +0530

    Fix typo in mongoose import and schema definition in notes.js

--- models/notes.js
+++ models/notes.js
@@ -1,10 +1,9 @@
-const mangoose = require('mongoose');
+const mongoose = require('mongoose');
 
-const noteSchema = new mangoose.Schema({
-    title:{type: String, required: true},
-    user:{type:mangoose.Schema.Types.ObjectId,ref: 'User',required: true},\
-    createdAt:{type:Date, default: Date.now},
+const noteSchema = new mongoose.Schema({
+  title: {type: String,required: true},
+  user: {type: mongoose.Schema.Types.ObjectId,ref: 'User',required: true},
+  createdAt: {type: Date,default: Date.now}
+});
 
-})
-
-module.exports = mangoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema);
